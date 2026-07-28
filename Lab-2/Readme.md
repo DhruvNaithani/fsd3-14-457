9e642e22d001f953aa08bf3d532fe403a469ecd7
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-07-28 10:12:40 +0530

    FSD-1

---
+++ Lab-2/Readme.md
@@ -0,0 +1,16 @@
+# CRUD
+c->create,
+r->retrieve,
+u->update,
+d->delete.
+# File system of NodeJs
+It allows JS code running outside the browser and interact directly to operating system
+## Common operations on file/folder
+- Reeading and writing files -> readfile(),writefile(),appendfile()
+- Directory management -> mkdir(),rmdir(),readdir()
+- Metadata/information -> stat(),lstat(),fstat()
+- Watching for changes -> watch(),watchfile(),unwatchfile()
+- Streaming Large Files -> createReadStream(),createWriteStream()
+- File Operations -> rename(),truncate(),unlink(),link(),systlink()
+
+- await can be used with any promise in async function. That async function will also accessed by await 
---
+++ Lab-2/index.js
@@ -0,0 +1,29 @@
+import {writeFile ,appendFile} from "fs/promises";
+import {readFile} from "fs/promises";
+
+// await writeFile("stud.txt","Ravikant Singh\nRoll Number:82");
+//console.log("File written");
+
+// const data = await readFile("stud.txt","utf-8");
+// console.log(`file contents :${data}`);
+
+const addContent = async (fname,content)=>{
+    await writeFile(fname,content);
+    console.log("file written");
+};
+const readContent= async (fname)=>{
+    const f_name= await readFile(fname,"utf-8");
+    return f_name;
+};
+const appendData =async (fname,content)=>{
+    await appendFile(fname,"\n"+content);
+    console.log("data appended");
+
+};
+await addContent("notes.txt","FS IS EASY IN JS");
+console.log("content:\n", await readContent("notes.txt"));
+await appendData("notes.txt","it can add update and read data");
+console.log("updated content:\n", await readContent("notes.txt"));
+
+
+
