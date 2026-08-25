6f08f0f01817b75e6d7901d9abd120c7b6246de3
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-08-25 09:36:45 +0530

    updated

--- lab3/prg1.js
+++ lab3/prg1.js
@@ -1,11 +1,21 @@
-import http from 'http';
-const server=http.createServer((req,res)=>{
-    const{url,method}=req;
-    res.setHeader('Content-Type','application/json');
-    
-    res.write("server is created");
-    res.end();
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  console.log("Welcome to Node JS");
+  console.log(req.url);
+  console.log("Request Method:");
+
+  console.log(req.method);
+
+  console.log("Request Header");
+  console.log(req.headers);
+  console.log("Socket Info");
+  //   console.log(req.socket);
+  console.log(req.headers.host);
+
+  res.end("hello");
 });
-server.listen(3001,()=>{
-    console.log("server listens");
-});
+
+const PORT = 4444;
+
+server.listen(PORT, () => console.log("Server is running..."));
--- lab3/prg2.js
+++ lab3/prg2.js
@@ -1,8 +1,11 @@
-import http from 'http';
-const server=http.createServer((req,res)=>{
-    res.write("prg2 is created");
-    res.end();
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  //   res.writeHead(500, {
+  //     "content-type": "text/html",
+  //   });
+
+  res.end("<h1>Welcome to Server</h1>");
 });
-server.listen(3002,()=>{
-    console.log("prg2 is working");
-});
+
+server.listen(4444, () => console.log("Server is running"));
