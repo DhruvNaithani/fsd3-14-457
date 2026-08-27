083e34787ba948a237fc3c71f2ad6e3e6bca8149
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-08-27 14:50:37 +0530

    updated

---
+++ lab3/prg8.js
@@ -0,0 +1,35 @@
+import http from 'http';  
+import { createReadStream } from 'fs';
+import { readFile } from 'fs/promises'; 
+const server =http.createServer(async(req,res)=>{
+    if(req.url==='/stream'){
+        const stream=createReadStream("big.txt",{encoding:"utf-8"});
+        stream.pipe(res);
+    } else if(req.url==='/normal'){
+        const text= await readFile("big.txt");
+        res.end(text);
+    } else if(req.url==='/product'){
+        res.setHeader("content-type","text/html");
+        res.statusCode=200;
+        const data=createReadStream("product.html");
+        data.pipe(res);
+    } else if(req.url==='/about'){
+        res.setHeader("content-type","text/html");
+        res.statusCode=200;
+        const data=createReadStream("about.html");
+        data.pipe(res);
+    }else if(req.url==='/home'){
+        res.setHeader("content-type","text/html");
+        res.statusCode=200;
+        const data=createReadStream("home.html");
+        data.pipe(res);
+    } else if(req.url==='/contact'){
+        res.setHeader("content-type","text/html");
+        res.statusCode=200;
+        const data=createReadStream("contact.html");
+        data.pipe(res);
+    } 
+});
+server.listen(4000,()=>{
+    console.log('server is listening');
+});
--- lab3/readme.md
+++ lab3/readme.md
@@ -3,3 +3,9 @@ Hyper Text Transfer Protocol
 html = hyper text markup language
 css = cascade style sheet
 npm = node package manager
+
+## API
+any api can be of four types 
+Get-->read
+Delete-->delete
+Api can be responsed by server with status code and JSON data api generally start with API/version.By default can check only get request to check other three request type like post put patch or delete we require frontend or third party api tester like postman,thunderclient,echoApi.
