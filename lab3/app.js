6812b955c41c8a9098ff9e14053313e334e70ffd
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-09-08 09:40:19 +0530

    sih internal

--- lab3/app.js
+++ lab3/app.js
@@ -1,10 +1,10 @@
 import http from 'http';
 const server=http.createServer();
 server.on("request",(req,res)=>{
-    res.setHeader("Content-Type", "text/html");
-    res.write("<p style='color:blue'>welcome to the server side programming");
-    res.write("<h2> nodemon is ttracing the file </h2>");
-    res.end();
+    // res.setHeader("Content-Type", "text/html");
+    // res.write("<p style='color:blue'>welcome to the server side programming");
+    // res.write("<h2> nodemon is ttracing the file </h2>");
+    res.end("<h1> SIH internal </h1>");
 });
 
 server.listen(5000,()=>{
