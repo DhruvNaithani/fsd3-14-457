db6ae4aa19d91188c3c4a296b5103b0d42621069
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-09-17 13:59:16 +0530

    basic routing with express server

--- lab5/app.js
+++ lab5/app.js
@@ -8,3 +8,21 @@ app.get("/" , (req,res)=>{
 app.listen(3000, ()=>{
     console.log("server 3000 is listening ");
 })
+
+app.post("/login",(req,res)=>{
+    res.send({msg:"enter user"});
+})
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:"update user "});
+})
+app.delete('/user/1',(req,res)=>{
+    res.send({msg:'remove user'});
+})
+app.get("/about",(req,res)=>{
+    res.send("<h2> we are fsd developer </h2>");
+})
+app.use((req,res)=>{
+    res.status(404).send("not found");
+})
+
+// app.get / app.post / app.put / app.delete / app.use
---
+++ lab5/lab5_readme.md
@@ -0,0 +1,6 @@
+# Express
+fast , unopiniated , minimalist web framework for node.js .
+1. ```npm init -y```
+2. update package.json.
+3. install nodemon : `npm i nodemon -D`
+4. `npm install express`
